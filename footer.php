66a6174687628cbbf27e32352dbdbe7fe306c5a6
John Lester <johnlesterbalucas.pamplona@gmail.com>
2022-01-24 20:04:07 +0700

    Added nav_menu

--- footer.php
+++ footer.php
@@ -5,8 +5,7 @@
             wp_nav_menu(
                 array(
                     'theme_location' => 'footer-menu',
-                    // 'menu' => 'Top bar',
-                    'menu_class' => 'footer-bar' // Binabago nya yung class name ng ul default name - menu
+                    'menu_class' => 'footer-bar'
                 )
             );
 
